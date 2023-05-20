b410bc59be8fc94dbc14b5322549c1f935738e96
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2023-05-21 01:19:30 +0900

    [feat] 기능 개발

--- fool_liar_game/lib/Screen/HomeScreen.dart
+++ fool_liar_game/lib/Screen/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:fool_liar_game/Screen/PlayScreen.dart';
 
@@ -7,9 +8,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var MenuType = ["음식", "영화"];
-  String selectedDropdown = '음식';
-  int count = 3;
+  var MenuType = ["음식", "영화", "직업", "동물", "장소", "나라"];
+  String? selectedValue;
+  int personCount = 3;
+  int foolCount = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -18,61 +20,171 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '인원',
-            ),
+            Text('게임 인원', style: TextStyle(fontSize: 20)),
             Container(
+              margin: EdgeInsets.only(bottom: 46),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   GestureDetector(
-                    child: Icon(Icons.remove),
+                    child: Icon(
+                      Icons.remove,
+                      size: 42,
+                    ),
                     onTap: () {
                       setState(() {
-                        if (count > 3) count--;
+                        if (personCount > 3) personCount--;
                       });
                     },
                   ),
                   Container(
-                    child: Text("$count"),
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    child: Text(
+                      "$personCount",
+                      style: TextStyle(fontSize: 36),
+                    ),
                   ),
                   GestureDetector(
-                    child: Icon(Icons.add),
+                    child: Icon(
+                      Icons.add,
+                      size: 42,
+                    ),
                     onTap: () {
                       setState(() {
-                        count++;
+                        personCount++;
                       });
                     },
                   )
                 ],
               ),
             ),
-            Text(
-              '주제',
+            Text('라이어 인원', style: TextStyle(fontSize: 20)),
+            Container(
+              margin: EdgeInsets.only(bottom: 46),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    child: Icon(
+                      Icons.remove,
+                      size: 42,
+                    ),
+                    onTap: () {
+                      setState(() {
+                        if (foolCount > 1) foolCount--;
+                      });
+                    },
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    child: Text(
+                      "$foolCount",
+                      style: TextStyle(fontSize: 36),
+                    ),
+                  ),
+                  GestureDetector(
+                    child: Icon(
+                      Icons.add,
+                      size: 42,
+                    ),
+                    onTap: () {
+                      setState(() {
+                        if (personCount - 1 > foolCount) foolCount++;
+                      });
+                    },
+                  )
+                ],
+              ),
             ),
-            DropdownButton(
-              value: selectedDropdown,
-              items: MenuType.map((String item) {
-                return DropdownMenuItem<String>(
-                  child: Text('$item'),
-                  value: item,
-                );
-              }).toList(),
-              onChanged: (dynamic value) {
-                setState(() {
-                  selectedDropdown = value;
-                });
-              },
-            ),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => PlayScreen(
-                              type: selectedDropdown, playerCount: count)));
+            Text('주제', style: TextStyle(fontSize: 20)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 26),
+              child: DropdownButtonFormField2(
+                decoration: InputDecoration(
+                  isDense: true,
+                  contentPadding: EdgeInsets.zero,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                isExpanded: true,
+                hint: const Text(
+                  '주제를 선택 해주세요',
+                  style: TextStyle(fontSize: 14),
+                ),
+                items: MenuType.map((item) => DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(
+                        item,
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    )).toList(),
+                validator: (value) {
+                  if (value == null) {
+                    return '주제를 선택 해주세요.';
+                  }
+                  return null;
                 },
-                child: Text("확인"))
+                onChanged: (value) {
+                  setState(() {
+                    selectedValue = value.toString();
+                  });
+                },
+                onSaved: (value) {
+                  selectedValue = value.toString();
+                },
+                buttonStyleData: const ButtonStyleData(
+                  height: 60,
+                  padding: EdgeInsets.only(left: 20, right: 10),
+                ),
+                iconStyleData: const IconStyleData(
+                  icon: Icon(
+                    Icons.arrow_drop_down,
+                    color: Colors.black45,
+                  ),
+                  iconSize: 30,
+                ),
+                dropdownStyleData: DropdownStyleData(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 26),
+              child: OutlinedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0))),
+                  ),
+                  onPressed: () {
+                    if (selectedValue == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text('주제를 선택 해주세요.'),
+                      ));
+
+                      return;
+                    }
+
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PlayScreen(
+                                type: selectedValue!,
+                                playerCount: personCount,
+                                foolCount: foolCount)));
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Text(
+                      "확인",
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                  )),
+            )
           ],
         ),
       ),
--- fool_liar_game/lib/Screen/PlayScreen.dart
+++ fool_liar_game/lib/Screen/PlayScreen.dart
@@ -1,10 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:fool_liar_game/Topic/Topic.dart';
 
 class PlayScreen extends StatefulWidget {
   final String type;
   final int playerCount;
+  final int foolCount;
 
-  PlayScreen({required this.type, required this.playerCount});
+  PlayScreen(
+      {required this.type, required this.playerCount, required this.foolCount});
 
   @override
   State<PlayScreen> createState() => _PlayScreenState();
@@ -13,30 +18,157 @@ class PlayScreen extends StatefulWidget {
 class _PlayScreenState extends State<PlayScreen> {
   bool isOpen = false;
   bool isGameDone = false;
+  bool keyWordOpen = false;
 
   int nowPlayer = 0;
-  int foolPlayerIndex = 0;
+  List<int> foolIndexList = [];
+
+  String topicWord = "";
+  String foolTopicWord = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    for (int index = 0; index < widget.foolCount; index++) {
+      var foolIndex = Random().nextInt(widget.playerCount);
+      if (!foolIndexList.contains(foolIndex)) foolIndexList.add(foolIndex);
+    }
+
+    List<String> topicList = mapList[widget.type]!;
+    topicWord = topicList.elementAt(Random().nextInt(topicList.length));
+    setFoolWord(topicList);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black45,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text("주제"),
-            Text(widget.type),
-            isOpen
-                ? Text("주제는 ${nowPlayer == foolPlayerIndex ? 1 : 0} 입니다!")
-                : GestureDetector(
-                    onSecondaryLongPress: () {},
-                    child: Container(
-                      child: Text("화면을 눌러주세요."),
+            Column(
+              children: [
+                Text(
+                  "주제",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  widget.type,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                )
+              ],
+            ),
+            Divider(),
+            (isOpen && nowPlayer < widget.playerCount)
+                ? Container(
+                    height: 200,
+                    child: Column(
+                      children: [
+                        Text("제시어",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 24)),
+                        Text(
+                            "${foolIndexList.contains(nowPlayer) ? foolTopicWord : topicWord}",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 28)),
+                        TextButton(
+                            onPressed: () {
+                              setState(() {
+                                isOpen = false;
+                                nowPlayer++;
+                                if (nowPlayer == widget.playerCount)
+                                  isGameDone = true;
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  borderRadius: BorderRadius.circular(36)),
+                              margin: EdgeInsets.only(top: 26),
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 18, horizontal: 24),
+                              child: Text(
+                                "확인완료!",
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.black),
+                              ),
+                            ))
+                      ],
                     ),
                   )
+                : keyWordOpen
+                    ? Column(
+                        children: [
+                          Text(
+                            "진짜 키워드",
+                            style: TextStyle(fontSize: 20, color: Colors.black),
+                          ),
+                          Text(
+                            topicWord,
+                            style: TextStyle(fontSize: 24, color: Colors.black),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 40),
+                            child: Text(
+                              "라이어 키워드",
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.black),
+                            ),
+                          ),
+                          Text(
+                            foolTopicWord,
+                            style: TextStyle(fontSize: 24, color: Colors.black),
+                          )
+                        ],
+                      )
+                    : GestureDetector(
+                        onLongPress: () {
+                          setState(() {
+                            if (!isGameDone) {
+                              isOpen = true;
+                            } else {
+                              keyWordOpen = true;
+                            }
+                          });
+                          print("능능?");
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.black54,
+                              borderRadius: BorderRadius.circular(16)),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 120, horizontal: 80),
+                          child: Text(
+                            isGameDone ? "각 키워드\n확인하기" : "2초 이상\n화면을 눌러주세요.",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18,
+                                color: Colors.white),
+                          ),
+                        ),
+                      )
           ],
         ),
       ),
     );
+  }
+
+  void setFoolWord(List<String> topicList) {
+    foolTopicWord = topicList.elementAt(Random().nextInt(topicList.length));
+
+    if (foolTopicWord == topicWord) setFoolWord(topicList);
   }
 }
